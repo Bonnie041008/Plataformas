d433ffa67410cfb8ac2cc3934779f89102db8d06
SalvaBernades <salvador.bernades@estudiantat.upc.edu>
2024-01-25 21:30:47 +0100

    tilin

---
+++ citm_desvj_project_template-A1_Assigment1_Template/citm_desvj_project_template-A1_Assigment1_Template/Game/Source/GameOver.cpp
@@ -0,0 +1,105 @@
+#include "App.h"
+#include "Input.h"
+#include "Textures.h"
+#include "Audio.h"
+#include "Render.h"
+#include "Window.h"
+#include "Scene.h"
+#include "Map.h"
+#include "Physics.h"
+#include "Defs.h"
+#include "Log.h"
+#include "ModuleFadeToBlack.h"
+#include "Menu.h"
+#include "Coin.h"
+#include "GameOver.h"
+
+
+#include "GuiControl.h"
+#include "GuiManager.h"
+
+GO::GO() : Module()
+{
+	name.Create("gameover");
+}
+
+// Destructor
+GO::~GO()
+{}
+
+// Called before render is available
+bool GO::Awake(pugi::xml_node& config)
+{
+	LOG("Loading Scene");
+	bool ret = true;
+
+	// iterate all objects in the scene
+	// Check https://pugixml.org/docs/quickstart.html#access
+
+
+	
+
+	return ret;
+}
+
+// Called before the first frame
+bool GO::Start()
+{
+
+	pantallaIntro = app->tex->Load("Assets/Pantallas/SWMG_PantallaGameOver.png");
+
+	return true;
+}
+
+// Called each loop iteration
+bool GO::PreUpdate()
+{
+	return true;
+}
+
+// Called each loop iteration
+bool GO::Update(float dt)
+{
+	
+	if (app->scene->boss->health == 0)
+	{
+		active = true;
+		app->entityManager->active = false;
+		app->map->active = false;
+		app->scene->active = false;
+		app->guiManager->active = false;
+		app->render->DrawTexture(pantallaIntro, 0, 0, false);
+	}
+
+
+	app->render->DrawTexture(pantallaIntro, 0, 0, false);
+
+	
+
+
+	return true;
+}
+
+// Called each loop iteration
+bool GO::PostUpdate()
+{
+	bool ret = true;
+	if (ext == true) {
+		ret = false;
+	}
+	if(app->input->GetKey(SDL_SCANCODE_ESCAPE) == KEY_DOWN)
+		ret = false;
+	if (app->menu->exitButton->exit == true) {
+		ret = false;
+	}
+	return ret;
+}
+
+
+// Called before quitting
+bool GO::CleanUp()
+{
+	LOG("Freeing Menu");
+
+	return true;
+}
