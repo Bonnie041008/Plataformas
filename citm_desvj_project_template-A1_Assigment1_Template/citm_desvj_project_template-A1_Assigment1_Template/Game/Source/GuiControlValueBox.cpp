10c46e6b216ebeea6103590752ea15baa8caf53c
Alex Becerra Garcia <abecerragarcia04@gmail.com>
2024-01-26 12:08:22 +0100

    h

--- citm_desvj_project_template-A1_Assigment1_Template/citm_desvj_project_template-A1_Assigment1_Template/Game/Source/GuiControlValueBox.cpp
+++ citm_desvj_project_template-A1_Assigment1_Template/citm_desvj_project_template-A1_Assigment1_Template/Game/Source/GuiControlValueBox.cpp
@@ -41,7 +41,7 @@ bool GuiControlValueBox::Update(float dt)
         }
        // app->render->DrawRectangle(bounds, 0, 0, 255, 255, true, false);
         app->render->DrawText(value.c_str(), bounds.x, bounds.y, bounds.w / 2, bounds.h,255, 255, 255);
-
+        app->render->DrawTexture(texture, 20, 0, false);
     }
 
 
